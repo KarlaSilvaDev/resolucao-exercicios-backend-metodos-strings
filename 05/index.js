5d9094a97344c6a631e01326919066134eaf89b8
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-16 17:50:27 -0300

    Ex05 - Esconder número do cartão de crédito

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,7 @@
 const numeroCartao = '1111222233334444';
+let numeroCartaoOculto = numeroCartao.slice(0,4) + numeroCartao.slice(-4).padStart(12,"*");
+console.log(numeroCartaoOculto);
+/*
+numeroCartao.slice(0,4): retorna os primeiros 4 dígitos do cartão (16 - 4 = 12 dígitos restantes)
+numeroCartao.slice(-4).padStart(12,"*"): pega os últimos 4 dígitos do cartão (slice(-4)) e coloca asteriscos à esquerda até que o comprimento total da string seja igual a 12 (.padStart(12,"*")). 
+*/
