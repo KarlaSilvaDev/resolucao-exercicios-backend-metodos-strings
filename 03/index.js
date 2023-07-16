41fd9d13fa954094484863f3cc9317acfe3e6812
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-16 16:53:47 -0300

    Ex03 - URL amigável

--- 03/index.js
+++ 03/index.js
@@ -1,1 +1,8 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+let urlAmigavel= texto.toLowerCase().replaceAll(" ", "-");
+console.log(urlAmigavel)
+
+/* 
+toLowerCase(): retorna o valor da string original convertido em letras minúsculas;
+replaceAll(" ", "-"): no lugar dos espaços existentes no texto, são colocados traços ("-").
+*/
