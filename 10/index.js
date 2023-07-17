1f884cdb992f706a8059d9e243ee6cdf01ff0d76
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-17 16:56:16 -0300

    Ex10 - Validação de Arquivos

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,21 @@
-const nomeArquivo = 'Foto da Familia.pdf';
+const nomeArquivo1 = 'Foto da Familia.pdf';
+const nomeArquivo2 = 'Foto da Familia.png';
+
+function validarArquivo(nomeArquivo){
+    let extensoesPermitidas = ["jpg", "jpeg", "gif", "png"];
+    let extensaoDoArquivo = nomeArquivo.slice(nomeArquivo.lastIndexOf(".") + 1);
+    let valido = false;
+    for (let extensaoPermitida of extensoesPermitidas){
+        if (extensaoPermitida === extensaoDoArquivo){
+            valido = true;
+            console.log("Arquivo válido");
+            break;
+        }
+    }
+    if (!valido){ 
+        console.log("Arquivo inválido");
+    }
+}
+
+validarArquivo(nomeArquivo1);
+validarArquivo(nomeArquivo2);
