b7e29e8e5b3391939edad6893066a7fd6201d2ce
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-17 16:34:14 -0300

    Ex08 - Remover pontuação

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,14 @@
 const cpf = "011.022.033-44";
+const cnpj = "12.345.678/0001-99";
+
+function removerPontuacao(documento){
+    let pontuacoes = [".", "-", "/"];
+    let documentoFormatado = documento;
+    for (let pontuacao of pontuacoes){
+        documentoFormatado = documentoFormatado.replaceAll(pontuacao,"");
+    }
+    console.log(documentoFormatado);
+}
+
+removerPontuacao(cpf);
+removerPontuacao(cnpj);
