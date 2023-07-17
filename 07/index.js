3b811dbf30c3688cb938768d5680b2da0962d8e7
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-17 16:20:58 -0300

    Ex07 - Validação de Email

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,17 @@
-const email = "aluno@cubos.academy";
+//POSSÍVEIS ENTRADAS 
+//const email = "aluno@cubos.academy";
+const emails = ["jose@cubos.academy", "jose@cubos.academy.br", "jose.messias@cubos.academy", "jose.messias@cubos.io", "jose@cubos", "jose.messias@cubos", "jose.messias@.", "jose.@cubos", ".@", "@.", "jose.messias@cubos.", ".messias@cubos.", ".messias@cubos"]; //transformei em array pra facilitar os testes de saídas
+
+//SISTEMA
+function validarEmail(email){
+    if (email[0] !== "." && email.slice(-1) !=="." && email.includes("@") && email.includes(".")){
+        console.log("Email válido.")
+    }else{
+        console.log("Email inválido.")
+    }
+}
+
+//SAÍDA
+for (let email of emails){
+    validarEmail(email);
+}
