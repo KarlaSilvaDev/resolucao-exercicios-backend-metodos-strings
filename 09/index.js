5a785c02af2a621ef91bbbe18051e26d4be15a31
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-17 16:46:18 -0300

    Ex09 - Gerador de Nickname

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,9 @@
 const nome = 'Guido Cerqueira';
+
+function gerarNickname(nome){
+    let nickname = "@" + nome.toLowerCase().replaceAll(" ","").slice(0, 12);
+    console.log(nickname);
+}
+
+gerarNickname(nome);
+
